0ad83c4d892e7e099a8b9ec63adb2d53b6a0431b
trentiscool <148014527+trentiscool@users.noreply.github.com>
2023-11-10 10:11:50 +1300

    Update deblurring.m - more readable

--- deblurring.m
+++ deblurring.m
@@ -1,32 +1,25 @@
-I = im2double(imread('blur_car.png'));
-%imshow(I);
-title('Original Image (courtesy of MIT)');
 
 
-%remove gaussian noise
-PSF = fspecial('gaussian',7,10);
-V = .0001;
-WT = zeros(size(I));
-WT(5:end-4,5:end-4) = 1;
-INITPSF = ones(size(PSF));
-[J, P] = deconvblind(I,INITPSF,20,10*sqrt(V),WT);
+% Deblur Car Image. 
+
+% Import Image of Car (Blury)
+image = im2double(imread('blur_car.png'));
+
+% Weiner Deconvolution set up
+LEN = 42;
+THETA = 19;
+point_spread_function = fspecial('motion', LEN, THETA);
+
+% Estimate SNR
+noise_var = 0.005;
+estimated_nsr = noise_var / var(image(:));
+
+% Apply Weiner 
+output_wnr = deconvwnr(image,point_spread_function,estimated_nsr);
+
+% Print Image 
+imshow(output_wnr)
 
 
 
 
-
-%blurred = imfilter(I, PSF, 'conv', 'circular');
-%figure, imshow(blurred)
-
-%noise_mean = 0;
-noise_var = 0.008;
-estimated_nsr_J = noise_var / var(J(:));
-LEN = 47;
-THETA = 20;
-i = 0;
-PSF_motion = fspecial('motion', LEN, THETA);
-wnr1 = deconvwnr(J, P, estimated_nsr_J);
-wnr3 = deconvwnr(wnr1, PSF_motion, 0.095);
-imshow(wnr3)
-
-%disp(LEN)
